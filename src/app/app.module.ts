4c23db09c4a30d2f72f436c165ea1728f3089204
tapiamcclung <rtapia@centrogeo.edu.mx>
2025-01-21 16:15:15 -0600

    Update angular core and cli to v18. Migrate from browser to application build system.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,34 +7,28 @@ import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
-//import { AngularFireModule } from '@angular/fire';
-//import { AngularFireAuthModule } from '@angular/fire/auth';
-//import { AngularFireStorageModule } from '@angular/fire/storage';
 import { environment } from './../environments/environment';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
-@NgModule({
-  declarations: [AppComponent, LayoutComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    SharedModule,
-    CoreModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    //AngularFireModule.initializeApp(environment.firebaseConfig),
-    //AngularFireAuthModule,
-    //AngularFireStorageModule,
-  ],
-  providers: [provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
-    provideCharts(withDefaultRegisterables())],
-  bootstrap: [AppComponent],
+@NgModule({ declarations: [AppComponent, LayoutComponent],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        SharedModule,
+        CoreModule,
+        BrowserAnimationsModule
+    ],
+    providers: [
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideFirestore(() => getFirestore()),
+        provideAuth(() => getAuth()),
+        provideCharts(withDefaultRegisterables()), provideHttpClient(withInterceptorsFromDi())
+    ]
 })
 export class AppModule {}
